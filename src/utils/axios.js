49a8797c3eb4b8079da84f227f768a85f615949c
chandan-002 <chandan.unicoglobal@gmail.com>
2022-12-01 23:28:11 +0530

    bse url changes

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 // Setting up base Url for fetching data
 const fetcher = axios.create({
-    baseURL: "http://localhost:5000/api/v1/admin",
+    // baseURL: "http://localhost:5000/api/v1/admin", 
+    baseURL: "https://paint-app.onrender.com/api/v1/admin",
     headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${typeof window !== 'undefined' && localStorage.getItem('token')}`,
