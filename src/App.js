f5580ef34999738523666c7f5af1bdcb1af9d141
chandan-002 <chandan.unicoglobal@gmail.com>
2022-12-01 23:40:52 +0530

    done

--- src/App.js
+++ src/App.js
@@ -13,10 +13,12 @@ import Register from './pages/Register'
 import Home from './pages/Home'
 import Paint from './pages/Paint'
 import FourOFour from './pages/404'
+import Landing from './pages/Landing'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" >
+      <Route path="/" element={<Landing />} />
       <Route path="login" element={<Login />} />
       <Route path="register" element={<Register />} />
       <Route path="home" element={localStorage.getItem('logged') ? <Home /> : <FourOFour />} />
